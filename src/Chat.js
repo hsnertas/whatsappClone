38e348725067fc651546beafb705cf5163c91069
hsnertas <hsnertas@gmail.com>
2020-10-01 14:06:23 -0400

    chat header added

--- src/Chat.js
+++ src/Chat.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import MoreVert from "@material-ui/icons/MoreVert";
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import AttachFile from "@material-ui/icons/AttachFile";
+import SidebarChat from "./SidebarChat";
 function Chat() {
     return (
         <div className="chat">
-            <h4>I am chatbar</h4>
+           <div className='chat__header'>
+               <Avatar/>
+               <div className="chat__headerInfo">
+                   <h3>Room name</h3>
+                   <p>Last seen at...</p>
+               </div>
+               <div className="chat__headerRight">
+               <IconButton>
+            <SearchOutlinedIcon />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+               </div>
+           </div>
         </div>
     )
 }
