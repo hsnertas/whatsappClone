7055562db5a5e1ce6c700c1a5fe87d974546eb7e
hsnertas <hsnertas@gmail.com>
2020-10-01 14:43:15 -0400

    chat body added

--- src/Chat.js
+++ src/Chat.js
@@ -26,6 +26,14 @@ function Chat() {
           </IconButton>
                </div>
            </div>
+           <div className="chat__body">
+             <p>
+               <span className="chat__name">Hasan</span>
+               This is a message
+    <span className="chat__timestamp">{new Date().toUTCString()}</span>
+               
+               </p>
+           </div>
         </div>
     )
 }
