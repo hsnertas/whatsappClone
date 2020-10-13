f24e1f7f71cd38622dd0205e9821f8f746e0f438
hsnertas <hsnertas@gmail.com>
2020-10-13 16:05:51 -0400

    input connected with backend

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import MoreVert from "@material-ui/icons/MoreVert";
@@ -6,8 +6,19 @@ import SearchOutlinedIcon from "@material-ui/icons/SearchOutlined";
 import AttachFile from "@material-ui/icons/AttachFile";
 import EmojiEmotionsOutlinedIcon from "@material-ui/icons/EmojiEmotionsOutlined";
 import MicIcon from "@material-ui/icons/Mic";
-
+import axios from './axios';
 function Chat({ messages }) {
+const [input, setInput]=useState("");
+const sendMessage= async (e)=>{
+  e.preventDefault();
+  await axios.post('/messages/new', {
+    message:input,
+    name:"Hasan",
+    timestamp:"just now",
+    received:false,
+  })
+}
+
   return (
     <div className="chat">
       <div className="chat__header">
@@ -36,17 +47,17 @@ function Chat({ messages }) {
           </p>
         ))}
 
-        <p className="chat__message chat__reciever">
+        {/* <p className="chat__message chat__reciever">
           <span className="chat__name">Hasan</span>
           This is a message
           <span className="chat__timestamp">{new Date().toUTCString()}</span>
-        </p>
+        </p> */}
       </div>
       <div className="chat__footer">
         <EmojiEmotionsOutlinedIcon />
         <form>
-          <input placeholder="Type a message" type="text" />
-          <button type="submit">Send a message</button>
+          <input value={input} onChange={(e)=>setInput(e.target.value)} placeholder="Type a message" type="text" />
+          <button onClick={sendMessage} type="submit">Send a message</button>
         </form>
         <MicIcon />
       </div>
